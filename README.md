686ae93cd24c1fb718684de6220d830b1d2ba946
BP <billpenn2005@outlook.com>
2025-06-02 17:46:38 +0800

    new file:   .gitignore 	modified:   0.osu 	modified:   README.md 	modified:   osupack.py 	modified:   requirements.txt

--- README.md
+++ README.md
@@ -1,23 +1,21 @@
 # OSU Packer
+* ## Introduction
+  * Packer for OSU Mania
 * ## Environment
   * requirements.txt
 * ## Usage
-  * Pack multiple songs into one .osz file from target folder. (Note that each folder in target folder can only contain one .osu file)
-  * Need to set pack name,creator name,target folder path
-  * Example
-    * folder structure:
+  * Pack multiple songs into one .osz file from a target folder.
+  * Provide the following command line arguments: --dir, --packname, --creator.
+  * Example:
+    * Folder structure:
         ```
         TARGETFOLDER
         ├───SONG1
         │       SONG1.osu
-        │
         └───SONG2
                 SONG2.osu
         ```
-    * code:
+    * Command:
         ```
-        (Cmd) set folderpath 'TARGETFOLDER'
-        (Cmd) set packname PACKNAME
-        (Cmd) set creator CREATOR
-        (Cmd) run
+        python osupack.py --dir "TARGETFOLDER" --packname "PACKNAME" --creator "CREATOR"
         ```
--- osupack.py
+++ osupack.py
@@ -1,4 +1,3 @@
-import cmd2
 import sys
 import argparse
 import os
@@ -11,9 +10,14 @@ def merge_name(song_name=str(),creator=str()):
 def get_songs(song_path):
     return os.listdir(song_path)
 
-def get_modified_beatmap_str(lines,cntr=int(),song_name=str(),creator=str(),pic=str(),aud=str(),pak=str(),tags=str()):
-    #lines=file.readlines()
-    new_name=merge_name(song_name,creator)
+def get_modified_beatmap_str(lines,cntr=int(),song_name=str(),creator=str(),pic=str(),aud=str(),pak=str(),tags=str(),pack_creator=str(),snu=str(),diff_name=str(),ispack=False,isperson_pack=False):
+    #print(song_name)
+    if (not ispack) and (not isperson_pack):
+        new_name=merge_name(song_name,creator)
+    elif ispack:
+        new_name=diff_name
+    elif isperson_pack:
+        new_name=merge_name(diff_name,creator)
     pack_name=pak
     ret=str()
     md=0
@@ -36,6 +40,8 @@ def get_modified_beatmap_str(lines,cntr=int(),song_name=str(),creator=str(),pic=
             ret+='BeatmapSetID:-1\n'
         elif i.find('Tags')!=-1:
             ret+='Tags:'+tags+'\n'
+        elif i.find('Creator')!=-1:
+            ret+='Creator:'+pack_creator+'\n'
         elif md==0:
             if i.find('[Events]')!=-1:
                 ret+='[Events]\n'
@@ -54,10 +60,8 @@ def get_modified_beatmap_str(lines,cntr=int(),song_name=str(),creator=str(),pic=
     return ret
 
 def get_pic_name(lines):
-    #lines=file.readlines()
     md=0
     for i in lines:
-        #print(i)
         if md==0:
             if i.find('[Events]')!=-1:
                 md=1
@@ -66,22 +70,27 @@ def get_pic_name(lines):
             if i[:2]=='//':
                 continue
             else:
-                #print(i)
                 isp=i.strip('\n')
                 isplit=isp.split(',')
                 return isplit[2].strip('"')
 
 def get_audio_name(lines):
-    #lines=file.readlines()
     for i in lines:
         if i.find('AudioFilename')!=-1:
             isp=i.strip('\n')
             isplit=isp.split(':')
             return isplit[-1].strip()
 
-def get_song_name(lines):
+def get_song_name_unicode(lines):
     for i in lines:
         if i.find('TitleUnicode')!=-1:
+            isp=i.strip('\n')
+            isplit=isp.split(':')
+            return isplit[-1].strip()
+        
+def get_song_name(lines):
+    for i in lines:
+        if i.find('Title')!=-1:
             isp=i.strip('\n')
             isplit=isp.split(':')
             return isplit[-1].strip()
@@ -99,83 +108,117 @@ def get_tags(lines):
             isp=i.strip('\n')
             isplit=isp.split(':')
             return isplit[-1].strip()
+        
+def get_BeatmapID(lines):
+    for i in lines:
+        if i.find('BeatmapID')!=-1:
+            isp=i.strip('\n')
+            isplit=isp.split(':')
+            return isplit[-1].strip()
+        
+def get_BeatmapSetID(lines):
+    for i in lines:
+        if i.find('BeatmapSetID')!=-1:
+            isp=i.strip('\n')
+            isplit=isp.split(':')
+            return isplit[-1].strip()
+
+def get_diffname(lines):
+    for i in lines:
+        if i.find('Version')!=-1:
+            isp=i.strip('\n')
+            isplit=isp.split(':')
+            return isplit[-1].strip()
 
 def zipDir(dirpath, outFullName):
-    """
-    压缩指定文件夹
-    :param dirpath: 目标文件夹路径
-    :param outFullName: 压缩文件保存路径+xxxx.zip
-    :return: 无
-    """
     zip = zipfile.ZipFile(outFullName, "w", zipfile.ZIP_DEFLATED)
     for path, dirnames, filenames in os.walk(dirpath):
-        # 去掉目标跟路径，只对目标文件夹下边的文件及文件夹进行压缩
         fpath = path.replace(dirpath, '')
- 
         for filename in filenames:
             zip.write(os.path.join(path, filename), os.path.join(fpath, filename))
     zip.close()
 
-
-class Packer(cmd2.Cmd):
-    def __init__(self):
-        super().__init__()
-        self.folderpath=str()
-        self.add_settable(cmd2.Settable('folderpath',str,'folder to pack',self))
-        self.packname=str()
-        self.add_settable(cmd2.Settable('packname',str,'name of pack',self))
-        self.creator=str()
-        self.add_settable(cmd2.Settable('creator',str,'name of creator',self))
-    run_parser = cmd2.Cmd2ArgumentParser()
-    @cmd2.with_argparser(run_parser)
-    def do_run(self,args):
-        self.poutput('running')
-        if not os.path.exists('tmp'):
-            os.mkdir('tmp')
-        song_list=get_songs(self.folderpath)
-        song_path=self.folderpath
-        cntr=1
-        if not os.path.exists(self.folderpath+'//delete'):
-            os.mkdir(self.folderpath+'//delete')
-        shutil.copy('0.osu',self.folderpath+'//delete//0.osu')
-        for i in song_list:
-            father_path=song_path+'//'+i
-            lst=os.listdir(father_path)
-            all_tags=''
-            for j in lst:
-                spl=j.split('.')
-                if spl[-1]=='osu':
-                    osu_file_name=father_path+'//'+j
-                    osu_file=open(osu_file_name,encoding='utf-8')
-                    lns=osu_file.readlines()
-                    all_tags+=get_tags(lns)
-            for j in lst:
-                spl=j.split('.')
-                if spl[-1]=='osu':
-                    osu_file_name=father_path+'//'+j
-                    osu_file=open(osu_file_name,encoding='utf-8')
-                    lns=osu_file.readlines()
-                    song_name=get_song_name(lns)
-                    creator=get_creator(lns)
-                    pic_name=get_pic_name(lns)
-                    audio_name=get_audio_name(lns)
-                    outfile_str=get_modified_beatmap_str(lns,cntr,song_name,creator,pic_name,audio_name,self.packname,all_tags)
-                    osu_file.close()
-                    #self.poutput(pic_name)
-                    #osu_file=open('tmp//'+str(cntr)+'.osu')
-                    #osu_file.write(outfile_str)
-                    #osu_file.close()
-                    with open('tmp//Various Artists - '+self.packname+' ('+self.creator+') '+'['+merge_name(song_name,creator)+']'+'.osu', mode="w", encoding="utf-8") as f:
-                        f.write(outfile_str)
-                    if os.path.exists(father_path+'//'+pic_name):
-                        shutil.copy(father_path+'//'+pic_name,'tmp//'+str(cntr)+'.'+pic_name.split('.')[-1])
-                    if os.path.exists(father_path+'//'+audio_name):
-                        shutil.copy(father_path+'//'+audio_name,'tmp//'+str(cntr)+'.'+audio_name.split('.')[-1])
-                    cntr+=1
-        zipDir('tmp',self.packname+'.osz')
-        shutil.rmtree('tmp')
-        self.poutput('finished')
+def pack_songs(folderpath, packname, creator, extratags=''):
+    print('running')
+    bbcode_file=open('bbcode.txt','a')
+    bbcode_file.write('[box=map list]\n')
+    link_template='https://osu.ppy.sh/beatmapsets/{}#mania/{}'
+    song_hyperlink_template='[url={}]{}[/url]'
+    mapper_link_template='https://osu.ppy.sh/users/{}'
+    mapper_hyperlink_template='[url={}]{}[/url]'
+    if(extratags is None):
+        extratags=''
+    if not os.path.exists('tmp'):
+        os.mkdir('tmp')
+    song_list=get_songs(folderpath)
+    song_path=folderpath
+    cntr=1
+    if not os.path.exists(folderpath+'//delete'):
+        os.mkdir(folderpath+'//delete')
+    shutil.copy('0.osu',folderpath+'//delete//0.osu')
+    all_tags=extratags
+    pack_mp={}
+    person_pack_mp={}
+    for i in song_list:
+        father_path=song_path+'//'+i
+        lst=os.listdir(father_path)
+        pack_mp[i]=False
+        person_pack_mp[i]=False
+        for j in lst:
+            spl=j.split('.')
+            if spl[-1]=='osu':
+                osu_file_name=father_path+'//'+j
+                osu_file=open(osu_file_name,encoding='utf-8')
+                lns=osu_file.readlines()
+                all_tags+=get_tags(lns)+' '
+                osu_file.close()
+            elif spl[-1]=='PACK':
+                pack_mp[i]=True
+            elif spl[-1]=='P_PACK':
+                person_pack_mp[i]=True
+                
+    with open('tags.txt','w') as f:
+        f.write(all_tags)
+    for i in song_list:
+        father_path=song_path+'//'+i
+        lst=os.listdir(father_path)
+        for j in lst:
+            spl=j.split('.')
+            if spl[-1]=='osu':
+                osu_file_name=father_path+'//'+j
+                osu_file=open(osu_file_name,encoding='utf-8')
+                lns=osu_file.readlines()
+                song_name=get_song_name(lns)
+                snu=get_song_name_unicode(lns)
+                creator_=get_creator(lns)
+                pic_name=get_pic_name(lns)
+                audio_name=get_audio_name(lns)
+                bm_id=get_BeatmapID(lns)
+                bms_id=get_BeatmapSetID(lns)
+                diffname=get_diffname(lns)
+                bbcode_file.write(song_hyperlink_template.format(link_template.format(bms_id,bm_id),song_name)+' by '+mapper_hyperlink_template.format(mapper_link_template.format(creator_),creator_)+'\n')
+                outfile_str=get_modified_beatmap_str(lns,cntr,song_name,creator_,pic_name,audio_name,packname,all_tags,creator,snu,diffname,pack_mp[i],person_pack_mp[i])
+                osu_file.close()
+                with open('tmp//Various Artists - '+packname+' ('+creator+') '+'['+merge_name(song_name,creator_)+']'+'.osu', mode="w", encoding="utf-8") as f:
+                    f.write(outfile_str)
+                if os.path.exists(father_path+'//'+pic_name):
+                    shutil.copy(father_path+'//'+pic_name,'tmp//'+str(cntr)+'.'+pic_name.split('.')[-1])
+                if os.path.exists(father_path+'//'+audio_name):
+                    shutil.copy(father_path+'//'+audio_name,'tmp//'+str(cntr)+'.'+audio_name.split('.')[-1])
+                cntr+=1
+    zipDir('tmp',packname+'.osz')
+    shutil.rmtree('tmp')
+    bbcode_file.write('[/box]\n')
+    bbcode_file.close()
+    print('finished')
 
 if __name__=='__main__':
-    packer=Packer()
-    sys.exit(packer.cmdloop())
+    parser = argparse.ArgumentParser(description='OSU Beatmap Packer')
+    parser.add_argument('--dir', type=str, required=True, help='folder to pack')
+    parser.add_argument('--packname', type=str, required=True, help='name of pack')
+    parser.add_argument('--creator', type=str, required=True, help='name of creator')
+    parser.add_argument('--extratags', type=str, required=False, help='extra tags')
+
+    args = parser.parse_args()
+    
+    pack_songs(args.dir, args.packname, args.creator,args.extratags)
